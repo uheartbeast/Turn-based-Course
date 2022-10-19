abe6b4631995bc5154a29007fbbe86ef66cbb302
Heartbeast <heartbeast.studios@gmail.com>
2022-10-19 14:03:34 -0600

    Always Yield Down

--- Battle/Battle.gd
+++ Battle/Battle.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 onready var player_battle_unit = $PlayerPosition/PlayerBattleUnit
 onready var enemy_battle_unit = $EnemyPosition/EnemyBattleUnit
+onready var animation_player = $AnimationPlayer
 
 func _ready() -> void:
+	yield(animation_player, "animation_finished")
 	player_battle_unit.melee_attack()
 
 func _unhandled_input(event : InputEvent) -> void:
