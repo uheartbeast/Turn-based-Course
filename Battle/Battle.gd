4e9ad705121518e12676215dd5bdf967808258b9
Heartbeast <heartbeast.studios@gmail.com>
2022-10-19 13:53:03 -0600

    A Basic Battle Unit

--- Battle/Battle.gd
+++ Battle/Battle.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+onready var player_battle_unit = $PlayerPosition/PlayerBattleUnit
+onready var enemy_battle_unit = $EnemyPosition/EnemyBattleUnit
+
+func _ready() -> void:
+	player_battle_unit.melee_attack()
+
 func _unhandled_input(event : InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
 		SceneStack.pop()
---
+++ Battle/BattleUnit.gd
@@ -0,0 +1,5 @@
+extends Node2D
+class_name BattleUnit
+
+func melee_attack() -> void:
+	print("Attack")
