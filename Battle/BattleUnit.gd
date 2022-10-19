ada3858b04e3f5c78912877e65a217936e1f619f
Heartbeast <heartbeast.studios@gmail.com>
2022-10-19 16:50:28 -0600

    Battle Animations on the Battle Unit

--- Battle/BattleUnit.gd
+++ Battle/BattleUnit.gd
@@ -1,5 +1,14 @@
 extends Node2D
 class_name BattleUnit
 
+export(PackedScene) var battle_animations_scene
+
+var battle_animations : BattleAnimations
+
+func _ready() -> void:
+	if battle_animations_scene is PackedScene:
+		battle_animations = battle_animations_scene.instance()
+		add_child(battle_animations)
+
 func melee_attack() -> void:
 	print("Attack")
