e12b74650d476f7b8725d87c26dac7727b36ce40
Heartbeast <heartbeast.studios@gmail.com>
2022-10-21 12:15:42 -0600

    Async Turn Pool Adjustment

--- AsyncTurnPool.gd
+++ AsyncTurnPool.gd
@@ -9,5 +9,6 @@ func add(node : Node) -> void:
 	active_nodes.append(node)
 
 func remove(node : Node) -> void:
+	if active_nodes.empty(): return
 	active_nodes.erase(node)
 	if active_nodes.empty(): emit_signal("turn_over")
