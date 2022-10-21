362d7ba2cd9ef4d1617244949e34f8a1ec0f140d
Heartbeast <heartbeast.studios@gmail.com>
2022-10-21 11:23:55 -0600

    Basic Class Stats

---
+++ ClassStats/ClassStats.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name ClassStats
+
+export(String) var name
+export(int) var max_health
+export(int) var attack
+export(int) var defense
+
+export(PackedScene) var battle_animations
+
+var level := 1
+var health := 1
