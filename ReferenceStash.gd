73b53c5ec1f67a5a318efdd137d2b8cf8c025f9b
Heartbeast <heartbeast.studios@gmail.com>
2022-10-24 11:18:23 -0600

    Resources and Memory

--- ReferenceStash.gd
+++ ReferenceStash.gd
@@ -2,3 +2,5 @@ extends Node
 
 var turnManager := TurnManager.new()
 var asyncTurnPool := AsyncTurnPool.new()
+
+var elizabethStats : PlayerClassStats = load("res://ClassStats/ElizabethClassStats.tres")
