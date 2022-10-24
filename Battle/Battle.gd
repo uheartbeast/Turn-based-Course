5a0481d405a627240b662336308c458558f52ad7
Heartbeast <heartbeast.studios@gmail.com>
2022-10-24 15:16:11 -0600

    Battle Camera Implementation

--- Battle/Battle.gd
+++ Battle/Battle.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+const ZOOM_IN = Vector2(0.75, 0.75)
+const ZOOM_DEFAULT = Vector2.ONE
+
 var turnManager : TurnManager = ReferenceStash.turnManager
 var asyncTurnPool : AsyncTurnPool = ReferenceStash.asyncTurnPool
 
@@ -13,6 +16,10 @@ onready var level_up_ui = $"%LevelUpUI"
 onready var battle_menu = $"%BattleMenu"
 onready var battle_camera = $BattleCamera
 
+onready var center_position : Vector2 = $CenterRoot/CenterPoint.rect_global_position
+onready var enemy_camera_position : Vector2 = get_battle_unit_camera_position(enemy_battle_unit)
+onready var player_camera_position : Vector2 = get_battle_unit_camera_position(player_battle_unit)
+
 func _ready() -> void:
 	player_battle_unit_info.stats = player_battle_unit.stats
 	enemy_battle_unit_info.stats = enemy_battle_unit.stats
@@ -21,6 +28,12 @@ func _ready() -> void:
 	turnManager.connect("enemy_turn_started", self, "_on_enemy_turn_started")
 	turnManager.start()
 	asyncTurnPool.connect("turn_over", self, "_on_async_turn_pool_turn_over")
+
+func get_battle_unit_camera_position(battle_unit : BattleUnit) -> Vector2:
+	var final_position := Vector2()
+	var start = Vector2(battle_unit.global_position.x, center_position.y)
+	final_position = start.move_toward(center_position, 32)
+	return final_position
 
 func battle_won() -> void:
 	timer.start(0.5)
@@ -50,7 +63,7 @@ func _on_ally_turn_started() -> void:
 	battle_menu.hide_menu()
 	match option:
 		BattleMenu.ACTION:
-			battle_camera.focus_target(enemy_battle_unit)
+			battle_camera.focus_target(enemy_camera_position, ZOOM_IN)
 			player_battle_unit.melee_attack(enemy_battle_unit)
 		BattleMenu.ITEM:
 			turnManager.advance_turn()
@@ -62,7 +75,9 @@ func _on_enemy_turn_started() -> void:
 		yield(battle_won(), "completed")
 		exit_battle()
 		return
+	battle_camera.focus_target(player_camera_position, ZOOM_IN)
 	enemy_battle_unit.melee_attack(player_battle_unit)
 
 func _on_async_turn_pool_turn_over() -> void:
+	yield(battle_camera.focus_target(center_position, ZOOM_DEFAULT), "completed")
 	turnManager.advance_turn()
