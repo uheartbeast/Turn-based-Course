c20c10cbec5569c7ddeead9fa3c04f39b05178b1
Heartbeast <heartbeast.studios@gmail.com>
2022-10-24 10:42:22 -0600

    Drawing the Battle Unit Level

--- UI/BattleUnitInfo.gd
+++ UI/BattleUnitInfo.gd
@@ -3,6 +3,7 @@ extends Control
 var stats : ClassStats setget set_stats
 
 onready var health_bar := $HealthBar
+onready var level_label := $LevelLabel
 
 func set_stats(value : ClassStats) -> void:
 	stats = value
@@ -12,6 +13,7 @@ func connect_stats() -> void:
 	if not stats is ClassStats: return
 	stats.connect("health_changed", self, "_on_stats_health_changed")
 	health_bar.set_bar(stats.health, stats.max_health)
+	level_label.text = "Level : " + str(stats.level)
 
 func _on_stats_health_changed() -> void:
 	health_bar.animate_bar(stats.health, stats.max_health)
