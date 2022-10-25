a4650498f7a585bea61aadaea823a99a27e9467a
Heartbeast <heartbeast.studios@gmail.com>
2022-10-25 13:27:08 -0600

    Apple Item

--- Items/Item.gd
+++ Items/Item.gd
@@ -4,6 +4,7 @@ class_name Item
 export(String) var name : String
 export(String, MULTILINE) var description : String
 export(int) var amount = 1 setget set_amount
+export(Texture) var overworld_sprite
 
 func set_amount(value : int) -> void:
 	amount = max(0, value)
