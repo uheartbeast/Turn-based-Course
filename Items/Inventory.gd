0cb483f0429b19ca9b4fb64cfd06c99e87f31368
Heartbeast <heartbeast.studios@gmail.com>
2022-10-25 11:14:02 -0600

    Remove Item Function

--- Items/Inventory.gd
+++ Items/Inventory.gd
@@ -16,3 +16,14 @@ func add_item(item : Item, amount : int = 1) -> void:
 	else:
 		items[item_index].amount += amount
 		emit_signal("item_changed", item_index, item)
+
+func remove_item(item : Item, amount : int = 1) -> Item:
+	var item_index = items.find(item)
+	if item_index == -1: return null
+	
+	item.amount -= amount
+	emit_signal("item_changed", item_index, item)
+	if item.amount <= 0:
+		items.erase(item)
+		emit_signal("item_removed", item_index)
+	return item
