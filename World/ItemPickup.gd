f35a124818077d19acb1235bbe0a595fabdbdb80
Heartbeast <heartbeast.studios@gmail.com>
2022-10-25 13:42:55 -0600

    Item Pickup

---
+++ World/ItemPickup.gd
@@ -0,0 +1,19 @@
+extends Interactable
+
+var inventory : Inventory = ReferenceStash.inventory
+
+export(Resource) var item : Resource setget set_item
+
+onready var sprite = $Sprite
+
+func set_item(value : Item) -> void:
+	item = value
+	call_deferred("set_sprite_texture", item)
+
+func set_sprite_texture(item) -> void:
+	sprite.texture = item.overworld_sprite
+
+func _run_interaction() -> void:
+	inventory.add_item(item)
+	Events.emit_signal("request_show_message", "You found a " + str(item.name) + ".\n"+item.description)
+	queue_free()
