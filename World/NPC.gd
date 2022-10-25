21c896942fa2abfb9e3fddae3072ce6c35ca0b11
Heartbeast <heartbeast.studios@gmail.com>
2022-10-25 14:50:06 -0600

    NPC

---
+++ World/NPC.gd
@@ -0,0 +1,12 @@
+extends Interactable
+class_name NPC
+
+export (Resource) var character : Resource
+
+func _run_interaction() -> void:
+	Events.emit_signal("request_show_dialog", "Aaaaaah! I have a head ache.", character)
+	yield(Events, "dialog_finished")
+	Events.emit_signal("request_show_dialog", "Can you find a potion?", character)
+	yield(Events, "dialog_finished")
+	Events.emit_signal("request_show_dialog", "I'll look around for you.", load("res://Characters/ElizabethCharacter.tres"))
+	yield(Events, "dialog_finished")
