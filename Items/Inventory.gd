43f06795513f63daffe03c5b6fc108f6073d6754
Heartbeast <heartbeast.studios@gmail.com>
2022-10-25 11:10:08 -0600

    Add Item Function

--- Items/Inventory.gd
+++ Items/Inventory.gd
@@ -2,3 +2,17 @@ extends Resource
 class_name Inventory
 
 export(Array, Resource) var items : Array = []
+
+signal item_added(item_inex, item)
+signal item_changed(item_index, item)
+signal item_removed(item_index)
+
+func add_item(item : Item, amount : int = 1) -> void:
+	var item_index := items.find(item)
+	if item_index == -1:
+		items.append(item)
+		item.amount = amount
+		emit_signal("item_added", items.size()-1, item)
+	else:
+		items[item_index].amount += amount
+		emit_signal("item_changed", item_index, item)
