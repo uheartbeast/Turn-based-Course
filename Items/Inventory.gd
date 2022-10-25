983d69677bd9cd915ad821db9b930299eba80148
Heartbeast <heartbeast.studios@gmail.com>
2022-10-25 11:02:28 -0600

    Inventory Resource

---
+++ Items/Inventory.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name Inventory
+
+export(Array, Resource) var items : Array = []
--- ReferenceStash.gd
+++ ReferenceStash.gd
@@ -4,3 +4,4 @@ var turnManager := TurnManager.new()
 var asyncTurnPool := AsyncTurnPool.new()
 
 var elizabethStats : PlayerClassStats = load("res://ClassStats/ElizabethClassStats.tres")
+var inventory : Inventory = load("res://Items/Inventory.tres")
