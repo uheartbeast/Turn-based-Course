b38c35596f7184d5c2986f10b57acc5e9f302f02
Heartbeast <heartbeast.studios@gmail.com>
2022-10-27 11:15:37 -0600

    UI Stack Implementation

--- UI/BattleMenuManager.gd
+++ UI/BattleMenuManager.gd
@@ -2,6 +2,8 @@ extends Control
 
 var elizabethStats : PlayerClassStats = ReferenceStash.elizabethStats
 
+var uiStack := UIStack.new()
+
 onready var battle_menu := $"%BattleMenu"
 onready var action_list := $"%ActionList"
 onready var item_list := $"%ItemList"
@@ -10,27 +12,19 @@ func _ready() -> void:
 	action_list.fill(elizabethStats.battle_actions)
 	item_list.fill(elizabethStats.inventory.items)
 	yield(battle_menu.show_menu(), "completed")
-	battle_menu.grab_focus()
+	uiStack.push(battle_menu)
 
 func _unhandled_input(event : InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		action_list.release_focus()
-		item_list.release_focus()
-		action_list.hide()
-		item_list.hide()
-		battle_menu.grab_focus()
+		uiStack.pop()
 
 func _on_BattleMenu_menu_option_selected(option : int) -> void:
 	match option:
 		BattleMenu.ACTION:
-			battle_menu.release_focus()
-			action_list.show()
-			action_list.grab_focus()
+			uiStack.push(action_list)
 		
 		BattleMenu.ITEM:
-			battle_menu.release_focus()
-			item_list.show()
-			item_list.grab_focus()
+			uiStack.push(item_list)
 
 func _on_ActionList_resource_selected(resource : BattleAction):
 	print(resource.name)
