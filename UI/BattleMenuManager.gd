409bef90209c009fb0f3806eb87c7d9fb2e7b5d5
Heartbeast <heartbeast.studios@gmail.com>
2022-10-27 14:19:47 -0600

    Run Polish

--- UI/BattleMenuManager.gd
+++ UI/BattleMenuManager.gd
@@ -38,6 +38,8 @@ func _on_BattleMenu_menu_option_selected(option : int) -> void:
 			uiStack.push(item_list)
 		
 		BattleMenu.RUN:
+			battle_menu.hide_menu()
+			battle_menu.release_focus()
 			emit_signal("battle_menu_resource_selected", RUN_BATTLE_ACTION)
 
 func _on_ActionList_resource_selected(resource : BattleAction) -> void:
