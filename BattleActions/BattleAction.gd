fdae5845b96db3ecb936eb5cd18cbd55f582e9af
Heartbeast <heartbeast.studios@gmail.com>
2022-10-27 13:51:21 -0600

    Info Menu Implementation

--- BattleActions/BattleAction.gd
+++ BattleActions/BattleAction.gd
@@ -2,3 +2,4 @@ extends Resource
 class_name BattleAction
 
 export(String) var name := ""
+export(String, MULTILINE) var description := ""
--- UI/BattleMenuManager.gd
+++ UI/BattleMenuManager.gd
@@ -4,10 +4,13 @@ var elizabethStats : PlayerClassStats = ReferenceStash.elizabethStats
 
 var uiStack := UIStack.new()
 
+var selected_resource : Resource
+
 onready var battle_menu := $"%BattleMenu"
 onready var action_list := $"%ActionList"
 onready var item_list := $"%ItemList"
 onready var context_menu = $"%ContextMenu"
+onready var info_menu = $"%InfoMenu"
 
 func _ready() -> void:
 	action_list.fill(elizabethStats.battle_actions)
@@ -29,13 +32,16 @@ func _on_BattleMenu_menu_option_selected(option : int) -> void:
 
 func _on_ActionList_resource_selected(resource : BattleAction) -> void:
 	uiStack.push(context_menu)
-	print(resource.name)
+	selected_resource = resource
 
 func _on_ItemList_resource_selected(resource : Item) -> void:
 	uiStack.push(context_menu)
-	print(resource.name)
+	selected_resource = resource
 
 func _on_ContextMenu_option_selected(option : int) -> void:
 	match option:
 		ContextMenu.USE: print("use")
-		ContextMenu.INFO: print("info")
+		ContextMenu.INFO:
+			if selected_resource is Item or selected_resource is BattleAction:
+				info_menu.text = selected_resource.description
+				uiStack.push(info_menu)
