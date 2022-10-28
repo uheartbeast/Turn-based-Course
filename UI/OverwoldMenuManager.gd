abd7548051e43bd5fc233d9ade4253894e7a0e88
Heartbeast <heartbeast.studios@gmail.com>
2022-10-28 15:02:31 -0600

    Overworld Menu Manager UI

---
+++ UI/OverwoldMenuManager.gd
@@ -0,0 +1,8 @@
+extends MarginContainer
+class_name OverworldMenuManager
+
+onready var overworld_menu := $"%OverworldMenu"
+onready var elizabeth_stats := $"%ElizabethStats"
+onready var item_list := $"%ItemList"
+onready var context_menu := $"%ContextMenu"
+onready var info_menu := $"%InfoMenu"
