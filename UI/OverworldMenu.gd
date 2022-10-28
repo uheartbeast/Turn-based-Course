0b2f829f0ae97257445cab06cf71c2e5a449b308
Heartbeast <heartbeast.studios@gmail.com>
2022-10-28 14:38:28 -0600

    Overworld Menu

--- UI/OverworldMenu.gd
+++ UI/OverworldMenu.gd
@@ -1,2 +1,19 @@
 extends FocusMenu
+class_name OverworldMenu
 
+enum {STATS, ITEMS, EXIT}
+
+signal option_selected(option)
+
+func _ready() -> void:
+	get_tree().paused = true
+	grab_focus()
+
+func _on_StatsButton_button_down():
+	emit_signal("option_selected", STATS)
+
+func _on_ItemsButton_button_down():
+	emit_signal("option_selected", ITEMS)
+
+func _on_ExitButton_button_down():
+	emit_signal("option_selected", EXIT)
