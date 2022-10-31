3c38842d13f4bf45ae9d1561bfcac1b001684831
Heartbeast <heartbeast.studios@gmail.com>
2022-10-31 13:53:59 -0600

    Level Swapper

---
+++ LevelSwapper.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var player : KinematicBody2D
+
+func stash_player(player_to_stash : KinematicBody2D) -> void:
+	player = player_to_stash
+	player.get_parent().remove_child(player)
+
+func level_swap(player_to_stash : KinematicBody2D, new_level_string : String) -> void:
+	stash_player(player_to_stash)
+	get_tree().change_scene(new_level_string)
+	drop_player()
+
+func drop_player() -> void:
+	yield(get_tree(), "idle_frame")
+	var parent := get_tree().current_scene
+	parent.add_child(player)
+	player.owner = parent
