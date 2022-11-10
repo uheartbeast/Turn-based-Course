a6b0c6151ca95d570cb5108335d20bb5c0e0a89d
Heartbeast <heartbeast.studios@gmail.com>
2022-11-09 17:18:39 -0700

    Load Game Function

--- SaveManager.gd
+++ SaveManager.gd
@@ -37,3 +37,34 @@ func save_game() -> void:
 	print(data_string)
 	file.store_string(data_string)
 	file.close()
+
+func load_game() -> void:
+	if file.open(TEST_PATH, File.READ) != OK: return
+	var data_string := file.get_as_text()
+	file.close()
+	
+	var load_data : Dictionary = JSON.parse(data_string).result
+	WorldStash.data = load_data.world_data
+	
+	var stats : PlayerClassStats = ReferenceStash.elizabethStats
+	stats.level = load_data.stats.level
+	stats.health = load_data.stats.health
+	stats.experience = load_data.stats.experience
+	
+	var inventory : Inventory = ReferenceStash.inventory
+	inventory.items.clear()
+	for item_data in load_data.inventory:
+		var item : Item = load(item_data.resource_path)
+		inventory.add_item(item, item_data.amount)
+	
+	if not ReferenceStash.player is KinematicBody2D:
+		var player = load("res://World/OverworldPlayer.tscn").instance()
+		LevelSwapper.player = player
+		ReferenceStash.player = player
+	
+	var player = ReferenceStash.player
+	player.last_door_connect = -1
+	yield(Transition.fade_to_color(Color.black), "completed")
+	LevelSwapper.level_swap(player, load_data.current_scene)
+	player.global_position = Vector2(load_data.player.x, load_data.player.y)
+	Transition.fade_from_color(Color.black)
